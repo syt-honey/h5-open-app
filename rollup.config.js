480646ae743898df8ecc4413255a51454213f1c8
qianzhu <36325249+syt-honey@users.noreply.github.com>
2022-05-26 18:29:28 +0800

    refactor: refactor build config

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,15 @@ module.exports = () => {
   return {
     input: "index.js",
     plugins: [terser()],
-    output: {
-      name: 'Hoa'
-    }
+    output: [
+      {
+        file: "./dist/hoa.min.js",
+        format: 'iife',
+        name: 'Hoa'
+      }, {
+        file: "./dist/hoa.esm.js",
+        format: 'esm',
+      }
+    ]
   }
 }
