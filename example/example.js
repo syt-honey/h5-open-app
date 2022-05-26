31175400332c1ce4464d036f004f947fe05d716c
qianzhu <36325249+syt-honey@users.noreply.github.com>
2022-05-26 15:02:38 +0800

    refactor: refactor example

--- example/example.js
+++ example/example.js
@@ -1,28 +1,30 @@
-try {
-  const { H5_APP } = Hoa;
-  const container = document.getElementById("app");
-
-  const dom = await H5_APP({
-    openTagConfig: {
-      appid: "",
-      extinfo: ""
-    },
-    wechatConfig: {
-      appId: "",
-      timestamp: 0,
-      nonceStr: "",
-      signature: ""
-    },
-    btnContainerStyle: "",
-    btnStyle: "",
-    config: {
-      debug: true
+(async function e () {
+  try {
+    const { H5_APP } = Hoa;
+    const container = document.getElementById("app");
+  
+    const dom = await H5_APP({
+      openTagConfig: {
+        appid: "",
+        extinfo: ""
+      },
+      wechatConfig: {
+        appId: "",
+        timestamp: 0,
+        nonceStr: "",
+        signature: ""
+      },
+      btnContainerStyle: "",
+      btnStyle: "",
+      config: {
+        debug: true
+      }
+    });
+  
+    if (dom !== -1) {
+      dom.mount(container);
     }
-  });
-
-  if (dom !== -1) {
-    dom.mount(container);
+  } catch(e) {
+    console.log(e);
   }
-} catch(e) {
-  console.log(e);
-}
+})();
